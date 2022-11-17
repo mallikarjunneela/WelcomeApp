e02e05fd67b1b73be6d0b00dcff8153e4132d407
Mallikarjun Neela <117260058+mallikarjunneela@users.noreply.github.com>
2022-11-17 15:56:14 +0530

    Update index.js

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -3,31 +3,37 @@ import {Component} from 'react'
 import './index.css'
 
 class Welcome extends Component {
-    state {
-        isSubscribe: false,
-    }
+  state = {
+    isSubscribed: false,
+  }
 
-    onSubscribe = () => {
-        this.setState = (prevState => ({isSubscribe: !prevState.isSubscribe}))
-    }
-    render getButtonText = () => {
-        const {isSubscribe} = this.state
-        
-        return isSubscribe ? 'Subscribed' : 'Subscribe'
-    }
+  onSubscribe = () => {
+    this.setState(prevState => ({isSubscribed: !prevState.isSubscribed}))
+  }
 
-    render() {
-        const buttonText = this.getButtonText()
+  getButtonText = () => {
+    const {isSubscribed} = this.state
 
-        return (
-            <div className="bg-container">
-            <h1 className="heading">Welcome</h1>
-            <p className="description">Thank You! Happy Learning</p>
-            <button className="subscribe-button" type="button" onClick={this.onSubscribe}>{buttonText}</button>
-            <button className="button" type="button" onClick={this.onSubscribe}>{buttonText}</button>
-            </div>
-        )
-    }
+    return isSubscribed ? 'Subscribed' : 'Subscribe'
+  }
+
+  render() {
+    const buttonText = this.getButtonText()
+
+    return (
+      <div className="app-container">
+        <h1 className="heading">Welcome</h1>
+        <p className="description">Thank you! Happy Learning</p>
+        <button
+          type="button"
+          className="subscribe-button"
+          onClick={this.onSubscribe}
+        >
+          {buttonText}
+        </button>
+      </div>
+    )
+  }
 }
 
 export default Welcome
